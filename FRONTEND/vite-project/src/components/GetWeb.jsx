c256566d2c3c0756711d8d99d268eb5d2cf71b8c
Huehuehue <paraskumarop@gmail.com>
2023-07-13 10:27:09 +0530

    web3

--- FRONTEND/vite-project/src/components/GetWeb.jsx
+++ FRONTEND/vite-project/src/components/GetWeb.jsx
@@ -8,7 +8,7 @@ async function GetWeb() {
               web3 = new Web3(window.ethereum);
               await window.ethereum.enable();
             } else if (typeof window.web3 !== `undefined`) {
-              web3 = new web3(window.web3);
+              web3 = new Web3(window.web3);
             } else {
               console.log("No wallet Found");
             }
